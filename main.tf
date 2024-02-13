31d52dd9d5a023347f3e1345628120f44c18ebfa
Chibuike Okoli <chibk.okoli@gmail.com>
2024-02-13 09:46:29 +0100

    added the needed files

---
+++ main.tf
@@ -0,0 +1,31 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    spotify = {
+      version = "~> 0.2.6"
+      source  = "conradludgate/spotify"
+    }
+  }
+}
+
+provider "spotify" {
+  api_key = var.spotify_api_key
+}
+
+data "spotify_search_track" "by_artist" {
+  artist = "Davido"
+}
+
+resource "spotify_playlist" "playlist" {
+  name        = "Choko Davido Playlist"
+  description = "This playlist was created by Choko"
+  public      = true
+
+  tracks = [
+    data.spotify_search_track.by_artist.tracks[0].id,
+    data.spotify_search_track.by_artist.tracks[1].id,
+    data.spotify_search_track.by_artist.tracks[2].id,
+  ]
+}
